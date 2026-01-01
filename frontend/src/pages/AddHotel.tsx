f0e6b6bd868760c6181667745a42c9a52b3afb61
Arpan Kumar <arpan.main@outlook.com>
2026-01-01 20:27:53 +0530

    added navigation after hotel saving

--- frontend/src/pages/AddHotel.tsx
+++ frontend/src/pages/AddHotel.tsx
@@ -3,13 +3,17 @@ import ManageHotelForm from "../forms/ManageHotelForm/ManageHotelForm";
 import { useAppContext } from "../contexts/AppContext";
 import * as apiClient from "../api-client";
 
+import { useNavigate } from "react-router-dom";
+
 const AddHotel = () => {
   const { showToast } = useAppContext();
+  const navigate = useNavigate();
 
   const mutation = useMutation({
     mutationFn: apiClient.addMyHotel,
     onSuccess: () => {
       showToast({ message: "Hotel Saved!", type: "SUCCESS" });
+      navigate("/my-hotels");
     },
     onError: () => {
       showToast({ message: "Error Saving Hotel!", type: "ERROR" });
